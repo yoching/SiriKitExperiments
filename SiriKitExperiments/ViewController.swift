a1120d19451c08a912fe272f22deddcaf36862ab
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2018-09-26 14:40:55 +0900

    Initial Commit

---
+++ SiriKitExperiments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriKitExperiments
+//
+//  Created by Yoshikuni Kato on 2018/09/26.
+//  Copyright © 2018 Yoshikuni Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SiriKitExperimentsTests/SiriKitExperimentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SiriKitExperimentsTests.swift
+//  SiriKitExperimentsTests
+//
+//  Created by Yoshikuni Kato on 2018/09/26.
+//  Copyright © 2018 Yoshikuni Kato. All rights reserved.
+//
+
+import XCTest
+@testable import SiriKitExperiments
+
+class SiriKitExperimentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
